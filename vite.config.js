12fb113287cf451cf0c889d6dcc68e00e88a8171
Jaemin0826 <shiibadog@gmail.com>
2025-07-13 04:32:38 +0900

    Add full source code for capstonePrototype0

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr"; // ✅ 추가
 
 export default defineConfig({
+  base: "/capstonePrototype0/",
   plugins: [react(), svgr()], // ✅ svgr 추가
 });
